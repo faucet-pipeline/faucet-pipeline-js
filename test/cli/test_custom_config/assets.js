fe90c0b455b123b1fedd10a362ac18b8d3a43cf7
FND <fnd@localhost.localdomain>
2018-07-07 11:13:55 +0200

    added bundle analysis
    caveats:
    * there might be performance implications:
      https://github.com/doesdev/rollup-plugin-analyzer/issues/3
    * no detailed report yet:
      https://github.com/doesdev/rollup-plugin-analyzer/issues/2
    * missing configuration options (cf. inline comment)

--- test/cli/test_custom_config/assets.js
+++ test/cli/test_custom_config/assets.js
@@ -5,7 +5,10 @@ let path = require("path");
 module.exports = {
 	js: [{
 		source: "./index.js",
-		target: "./dist/bundle.js"
+		target: "./dist/bundle.js",
+		reporting: {
+			threshold: 100
+		}
 	}],
 	plugins: {
 		js: path.resolve(__dirname, "../../..")
--- test/cli/test_custom_config/expected.js
+++ test/cli/test_custom_config/expected.js
@@ -5,6 +5,22 @@ if(typeof global === "undefined" && typeof window !== "undefined") {
 	window.global = window;
 }
 
-// N/A
+// lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+// tempor incididunt ut labore et dolore magna aliqua
+// ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+// aliquip ex ea commodo consequat
+// duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+// eu fugiat nulla pariatur
+// excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+// deserunt mollit anim id est laborum
+//
+// lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+// tempor incididunt ut labore et dolore magna aliqua
+// ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+// aliquip ex ea commodo consequat
+// duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+// eu fugiat nulla pariatur
+// excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+// deserunt mollit anim id est laborum
 
 }());
