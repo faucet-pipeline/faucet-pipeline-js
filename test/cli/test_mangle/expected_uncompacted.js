2c798a26f4e5c45b692fe6281a5b821e194ed61d
FND <fnd@localhost.localdomain>
2019-02-03 13:13:16 +0100

    extended compacting options
    note that the terser dependency resides in a separate meta-package:
    since it's a fairly heavy dependency, so we don't wanna put an extra
    burden on all users (NB: faucet-jsmin intentionally doesn't depend on
    faucet-js itself because it's not intended as a stand-alone package)

---
+++ test/cli/test_mangle/expected_uncompacted.js
@@ -0,0 +1,19 @@
+(function () {
+'use strict';
+
+// dolor sit amet
+var util = "UTIL";
+
+// lorem ipsum
+function info() {
+	console.log(`[…] ${util}`);
+}
+
+// dolor sit amet
+function help() {
+	info();
+}
+
+help();
+
+}());
