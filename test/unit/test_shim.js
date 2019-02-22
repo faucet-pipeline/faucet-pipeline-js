1d876179239f5efb5ab3c3add126dee580445848
FND <fnd@localhost.localdomain>
2019-02-22 15:18:00 +0100

    discarded workaround for legacy Node
    no longer needed because we dropped support for Node v6
    
    this effectively reverts c2ed8a46aa60e731f0fd204eb3a3f8732aa70252

--- test/unit/test_shim.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// skip tests using modern syntax on legacy versions of Node
-// NB: this is a workaround until we can drop support for Node v6
-//     <https://github.com/faucet-pipeline/faucet-pipeline-core/issues/49>
-let major = process.version.split(".")[0].substr(1);
-if(parseInt(major, 10) > 6) {
-	require("./_test_virtual");
-}
