a8c584fcc299e7db8833c0d9e6bc04537aa73802
Lucas Dohmen <lucas@dohmen.io>
2022-02-21 18:28:51 +0100

    Fix CLI Tests

--- test/cli/test_jsx/expected.js
+++ test/cli/test_jsx/expected.js
@@ -4,9 +4,28 @@ function _classCallCheck(instance, Constructor) {
   }
 }
 
-var MyComponent = function MyComponent() {
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  Object.defineProperty(Constructor, "prototype", {
+    writable: false
+  });
+  return Constructor;
+}
+
+var MyComponent = /*#__PURE__*/_createClass(function MyComponent() {
   _classCallCheck(this, MyComponent);
-};
+});
 
 var el = createElement(MyComponent, {
   type: "dummy"
